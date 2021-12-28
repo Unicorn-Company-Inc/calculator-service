f465e3b830bb645ea6128e0175823268566f58ee
Nico Franke <nico.franke01@gmail.com>
2021-12-28 19:40:43 +0100

    :white_check_mark: Add mwst controller integration tests

--- src/mwst/mwst.controller.spec.ts
+++ src/mwst/mwst.controller.spec.ts
@@ -1,18 +1,41 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MwstController } from './mwst.controller';
+import { MwstService } from './mwst.service';
 
 describe('MwstController', () => {
   let controller: MwstController;
+  let service: MwstService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MwstController],
+      providers: [MwstService],
     }).compile();
 
     controller = module.get<MwstController>(MwstController);
+    service = module.get<MwstService>(MwstService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('getMwst', () => {
+    it('should return mwst', async () => {
+      const result = 19;
+      const params = { price: 100 };
+      const query = { percentage: undefined };
+      jest.spyOn(service, 'calculateMwst').mockImplementation(() => result);
+
+      expect(await controller.getMwst(params, query)).toBe(result);
+    });
+    it('should return mwst with custom percentage', async () => {
+      const result = 1;
+      const params = { price: 100 };
+      const query = { percentage: 1 };
+      jest.spyOn(service, 'calculateMwst').mockImplementation(() => result);
+
+      expect(await controller.getMwst(params, query)).toBe(result);
+    });
   });
 });
